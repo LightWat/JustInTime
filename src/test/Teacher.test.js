e7896c9456dca16afe7719d5020b5181f7dbe473
LightWat <dennislai120@gmail.com>
2022-06-13 12:00:30 +0800

    fix: 修正 TeacherSelectCourse 單元測試問題
    修正 TeacherSelectCourse 單元測試問題
    將單元測試放到 test folder 裡
    
    impact: high

--- src/test/Teacher.test.js
+++ src/test/Teacher.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Time from "./Time";
+import Teacher from "../components/Teacher";
 import '@testing-library/jest-dom/extend-expect'
 
 
 describe("學生可以查看老師提供的遠距課連結", () => {
 
-    test("有開關上課時間按鈕", () => {
-        render(<Time/>);
+    test("有開關老師名字按鈕", () => {
+        render(<Teacher/>);
         const button = screen.getByRole('button');
         expect(button).toBeTruthy();
     });
---
+++ src/test/TeacherSelectCourse.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+
+import TeacherSelectCourse from "../components/TeacherSelectCourse";
+import {NoticeProvider} from "../context/notice-hooks";
+
+
+describe("供老師放上遠距課連結", () => {
+
+  test("有提供輸入框", () => {
+    render(
+      <NoticeProvider>
+        <TeacherSelectCourse userId={"iamteacher"} />
+      </NoticeProvider>
+    );
+
+    const input = screen.getByRole("textbox");
+
+    expect(input).toBeTruthy();
+  });
+
+});
